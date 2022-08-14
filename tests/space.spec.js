46cc04e3352ac52c8b4c848b092a8ea5b5d97a7b
Test_project02 <https://github.com/VladPritzker/Test_project02.git>
2022-08-14 19:45:08 -0400

    commit 2

---
+++ tests/space.spec.js
@@ -0,0 +1,19 @@
+// describe('Space test suite', () => {
+//     test('My Space Test', () => {
+//         expect(true).toEqual(true);
+//     });
+// });
+
+const request = require('supertest');
+const app = require("../app");
+
+describe('Space test suite', () => {
+    it('tests /destinations endpoints', async() => {
+        const response = await request(app).get("/space/destinations");
+        expect(response.body).toEqual(["Mars", "Moon", "Earth", "Mercury", "Venus", "Jupiter"]);
+        expect(response.body).toHaveLength(6);
+        expect(response.statusCode).toBe(200);
+        // Testing a single element in the array
+        expect(response.body).toEqual(expect.arrayContaining(['Earth']));
+
+    });
--- tests/space.test.ts
+++
@@ -1,5 +0,0 @@
-describe('Space test suite', () => {
-    test('My Space Test', () => {
-        expect(true).toEqual(true);
-    });
-});
